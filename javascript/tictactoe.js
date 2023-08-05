61fa86340982beab265548d6817228fbd09c04f8
SaffetGokcenSen <saffetgokcensen@hotmail.com>
2023-08-05 15:55:51 +0300

    the board dimension to be selected by the players

--- javascript/tictactoe.js
+++ javascript/tictactoe.js
@@ -24,8 +24,43 @@ const theBoard = (() => {
 const gameController = (() => {
   let theDim;
   const getBoardDim = () => {
+    const theBody = document.body;
+
+    const selectionDiv = document.createElement('div');
+    theBody.appendChild(selectionDiv);
+    theBody.style.display = 'flex';
+    theBody.style.justifyContent = 'center';
+
+    const pText = document.createElement('p');
+    pText.textContent = 'Please choose the board dimension.';
+    selectionDiv.appendChild(pText);
+
+    let p1; let p2; let p3; let radioButton1; let radioButton2; let radioButton3;
+    let label1; let label2; let label3;
+    const pArray = [p1, p2, p3];
+    const radioButtonArray = [radioButton1, radioButton2, radioButton3];
+    const labelArray = [label1, label2, label3];
+    const dimArray = ['3x3', '5x5', '7x7'];
+    for (let i = 0; i < 3; i += 1) {
+      pArray[i] = document.createElement('p');
+      pArray[i].style.display = 'flex';
+      pArray[i].style.justifyContent = 'center';
+      radioButtonArray[i] = document.createElement('input');
+      labelArray[i] = document.createElement('label');
+      radioButtonArray[i].type = 'radio';
+      radioButtonArray[i].id = dimArray[i];
+      radioButtonArray[i].value = dimArray[i];
+      radioButtonArray[i].name = 'dimension';
+      labelArray[i].htmlFor = dimArray[i];
+      labelArray[i].textContent = dimArray[i];
+      pArray[i].appendChild(labelArray[i]);
+      pArray[i].appendChild(radioButtonArray[i]);
+      selectionDiv.appendChild(pArray[i]);
+    }
   };
 
   return { getBoardDim, theDim };
 }
 )();
+
+gameController.getBoardDim();
